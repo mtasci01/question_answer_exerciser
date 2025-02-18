f2d794a028f96b2a50af9bf431c75713ba35c7b2
Marco Tascini <tascinimarco@tiscali.it>
2025-02-18 23:46:56 +0100

    init project

---
+++ src/main/java/com/mt/question_answer_exerciser/QuestionAnswerExerciserApplication.java
@@ -0,0 +1,13 @@
+package com.mt.question_answer_exerciser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAnswerExerciserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAnswerExerciserApplication.class, args);
+	}
+
+}
